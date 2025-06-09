4280dba205b10f1473070265ca4efbcd3d54f48c
aBugInTheSystem <ekom338.com@gmail.com>
2025-06-09 15:42:53 -0400

    Expense schema created

--- src/main/java/com/ekom/backend/Model/Expense.java
+++ src/main/java/com/ekom/backend/Model/Expense.java
@@ -1,4 +1,12 @@
 package com.ekom.backend.Model;
 
 public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private Double amount;
+    private ExpenseType category;
+    private LocalDate date;
+
 }
