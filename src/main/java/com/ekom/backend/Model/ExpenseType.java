c52b5dc7d203c67ed9d814c5e484c2f012b1f429
aBugInTheSystem <ekom338.com@gmail.com>
2025-06-09 16:28:25 -0400

    Enum for the expense type created.

---
+++ src/main/java/com/ekom/backend/Model/ExpenseType.java
@@ -0,0 +1,18 @@
+package com.ekom.backend.Model;
+
+public enum ExpenseType {
+    RENT,
+    UTILITIES,
+    GROCERIES,
+    TRANSPORTATION,
+    HEALTHCARE,
+    INSURANCE,
+    DINING_OUT,
+    ENTERTAINMENT,
+    GYM,
+    TRAVEL,
+    SHOPPING,
+    OTHER
+
+
+}
